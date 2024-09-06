f3923bb8804145706d207cd66fe983a47cbbe53d
Yann Bodiguel <yann.bodiguel@etu.umontpellier.fr>
2024-09-06 09:32:15 +0200

    deuxième commit car j'y connais rien à git

--- src/main/java/fr/iut/editeur/commande/CommandeClear.java
+++ src/main/java/fr/iut/editeur/commande/CommandeClear.java
@@ -1,4 +1,22 @@
 package fr.iut.editeur.commande;
 
-public class CommandeClear {
+import fr.iut.editeur.document.Document;
+
+import static java.lang.Integer.parseInt;
+
+public class CommandeClear extends CommandeDocument {
+
+    public CommandeClear(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 1) {
+            System.err.println("Format attendu : clear");
+            return;
+        }
+        this.document.clear();
+        super.executer();
+    }
 }
